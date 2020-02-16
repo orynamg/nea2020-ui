68e1207cc82e955a8a487ebecee7100ca946077b
Oryna Goichuk <arina.goichuk@gmail.com>
2020-02-16 23:12:06 +0000

    summary

--- src/News.js
+++ src/News.js
@@ -1,63 +1,81 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Popover from "react-bootstrap/Popover"
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Badge from "react-bootstrap/Badge";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Overlay from "react-bootstrap/Overlay"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons'
 
 const CategoryBadge = ({categoryId}) => {
     const categories = ["business", "entertainment", "health", "tech & science", "environment", "lgbt", "youth"]
     const variants = ["primary", "danger", "success", "secondary", "warning", "info", "dark", "light"]
     return (<Badge pill variant={variants[categoryId]}>{categories[categoryId]}</Badge>)
-} 
+}
 
-const News = ({news}) => (
-    // <Jumbotron>
-    // {/* <div className="item"> */}
-    //     <div>{news.headline}</div>
-    //     <div>{news.source}</div>
-    //     <a href={news.url}>link</a>
-    //     <div>{news.created_at}</div>
-    //     <div>{news.category_id}</div>
-    //     <div>{news.event_id}</div>
-    // {/* </div> */}
-    // </Jumbotron>
+const News = ({news}) => {
+    const [show, setShow] = React.useState(false);
+    const [target, setTarget] = React.useState(null);
+    const ref = React.useRef(null);
+  
+    const handleClick = event => {
+      setShow(!show);
+      setTarget(event.target);
+    };
+    
+    return (
+        <Card style={{ width: "30rem" }}>
+            <Card.Body>
+                <Card.Title>
+                    <div style={{textAlign:"right"}}>
+                        <CategoryBadge categoryId={news.category_id}></CategoryBadge>
+                    </div>
+                </Card.Title>
+                <blockquote className="blockquote mb-0" onClick={handleClick}>
+                    <p>
+                        <a href={news.url}>{news.headline}</a>
+                        {/* <FontAwesomeIcon icon={faFileAlt} color="gray" onClick={handleClick} /> */}
+                    </p>
+                    <footer className="blockquote-footer">
+                        {news.source} on <cite title="Source Title">
+                            {(new Date(news.created_at)).toLocaleDateString()}
+                        </cite>
+                    </footer>
+                </blockquote>
 
-    <Card style={{ width: "30rem" }}>
-    <Card.Body>
-        <Card.Title>
-            <div style={{textAlign:"right"}}>
-                <CategoryBadge categoryId={news.category_id}></CategoryBadge>
-            </div>
-            {/* {news.source} */}
-        </Card.Title>
-        {/* <Card.Text>
-            {news.headline}
-        </Card.Text>
-        <Button variant="primary">Open news</Button> */}
-        <blockquote className="blockquote mb-0">
-            <p>
-                <a href={news.url}>{news.headline}</a>
-            </p>
-            <footer className="blockquote-footer">
-                {news.source} on <cite title="Source Title">
-                    {(new Date(news.created_at)).toLocaleDateString()}
-                </cite>
-            </footer>
-        </blockquote>
-    </Card.Body>
-    {/* <Card.Footer>
-      <small className="text-muted">Category: </small>
-      <Badge pill variant="success">business</Badge>
-    </Card.Footer> */}
-    </Card>
-)
+                <Overlay
+                    show={show}
+                    target={target}
+                    placement="left"
+                    container={ref.current}
+                    containerPadding={20}
+                    rootClose
+                >
+                    <Popover id="popover-contained">
+                        <Popover.Title as="h3">Summary</Popover.Title>
+                        <Popover.Content>
+                            {news.summary}
+                        </Popover.Content>
+                    </Popover>
+                </Overlay>
+
+            </Card.Body>
+            {/* <Card.Footer>
+                <FontAwesomeIcon icon={faFileAlt} color="gray" onClick={handleClick} />
+                <Button variant="Secondary" onClick={handleClick}>Summary</Button>
+            </Card.Footer> */}
+        </Card>
+    )
+}
 
 const NewsList = ({list}) => (
     <ul>
       {list.map(item => (
-        <News key={item.id} news={item} />
+        <div className="newsItem"><News key={item.id} news={item} /></div>
       ))}
     </ul>
-  );
+)
 
 export default NewsList;
